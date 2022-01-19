fffd3dd10730ab9da871416cf380fe3d295f521d
Lucas1024b <lucasribeiro1024b@gmail.com>
2022-01-19 10:59:23 -0300

    work: Visualização de exercicios.

---
+++ exercicio.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Exercício</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/estilo.css">
+</head>
+<body>
+  <header class="cabecalho">
+    <h1>Curso PHP</h1>
+    <h2>Visualização do Exercício</h2>
+  </header>
+  <main class="principal">
+    <div class="conteudo">
+
+    </div>
+  </main>
+  <footer class="rodape">
+    COD3R & Alunos © <?= date('Y')?>
+  </footer>
+</body>
+</html>
